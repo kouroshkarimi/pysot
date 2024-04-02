fba9afd679a7476f9349593b5abf80f6029afe9c
kouroshkarimi <thekouroshkarimi@gmail.com>
2024-04-02 09:42:24 +0330

    optimize get_subwindow function to set a stable time in tracking and be real-time

--- pysot/tracker/siamrpn_tracker.py
+++ pysot/tracker/siamrpn_tracker.py
@@ -11,6 +11,95 @@ import torch.nn.functional as F
 from pysot.core.config import cfg
 from pysot.utils.anchor import Anchors
 from pysot.tracker.base_tracker import SiameseTracker
+import cv2
+import torch
+import time
+
+def to_torch(ndarray):
+    if type(ndarray).__module__ == 'numpy':
+        return torch.from_numpy(ndarray)
+    elif not torch.is_tensor(ndarray):
+        raise ValueError("Cannot convert {} to torch tensor"
+                         .format(type(ndarray)))
+    return ndarray
+
+
+def im_to_torch(img):
+    img = np.transpose(img, (2, 0, 1))  # C*H*W
+    img = to_torch(img).float()
+    return img
+
+
+def cxywh2xyxy(box):
+    box = np.array(box, dtype=np.float32)
+    return np.concatenate([
+        box[..., [0]] - (box[..., [2]] - 1) / 2, box[..., [1]] -
+        (box[..., [3]] - 1) / 2, box[..., [0]] +
+        (box[..., [2]] - 1) / 2, box[..., [1]] + (box[..., [3]] - 1) / 2
+    ],
+                          axis=-1)
+
+def get_subwindow_tracking_(im,
+                           pos,
+                           model_sz,
+                           original_sz,
+                           avg_chans=(0, 0, 0),
+                           mask=None):
+    r"""
+    Get subwindow via cv2.warpAffine
+
+    Arguments
+    ---------
+    im: numpy.array
+        original image, (H, W, C)
+    pos: numpy.array
+        subwindow position
+    model_sz: int
+        output size
+    original_sz: int
+        subwindow range on the original image
+    avg_chans: tuple
+        average values per channel
+    mask: numpy.array
+        mask, (H, W)
+
+
+    Returns
+    -------
+    numpy.array
+        image patch within _original_sz_ in _im_ and  resized to _model_sz_, padded by _avg_chans_
+        (model_sz, model_sz, 3)
+    """
+    crop_cxywh = np.concatenate(
+        [np.array(pos), np.array((original_sz, original_sz))], axis=-1)
+    crop_xyxy = cxywh2xyxy(crop_cxywh)
+    # warpAffine transform matrix
+    M_13 = crop_xyxy[0]
+    M_23 = crop_xyxy[1]
+    M_11 = (crop_xyxy[2] - M_13) / (model_sz - 1)
+    M_22 = (crop_xyxy[3] - M_23) / (model_sz - 1)
+    mat2x3 = np.array([
+        M_11,
+        0,
+        M_13,
+        0,
+        M_22,
+        M_23,
+    ]).reshape(2, 3)
+    im_patch = cv2.warpAffine(im,
+                              mat2x3, (model_sz, model_sz),
+                              flags=(cv2.INTER_LINEAR | cv2.WARP_INVERSE_MAP),
+                              borderMode=cv2.BORDER_CONSTANT,
+                              borderValue=tuple(map(int, avg_chans)))
+    if mask is not None:
+        mask_patch = cv2.warpAffine(mask,
+                                    mat2x3, (model_sz, model_sz),
+                                    flags=(cv2.INTER_NEAREST
+                                           | cv2.WARP_INVERSE_MAP))
+        return im_patch, mask_patch
+    #cv2.imshow('croped_temp', im_patch)
+    #cv2.waitKey(1)
+    return im_to_torch(im_patch)
 
 
 class SiamRPNTracker(SiameseTracker):
@@ -56,7 +145,7 @@ class SiamRPNTracker(SiameseTracker):
 
     def _convert_score(self, score):
         score = score.permute(1, 2, 3, 0).contiguous().view(2, -1).permute(1, 0)
-        score = F.softmax(score, dim=1).data[:, 1].cpu().numpy()
+        score = F.softmax(score, dim=1).data[:, 1].detach().cpu().numpy()
         return score
 
     def _bbox_clip(self, cx, cy, width, height, boundary):
@@ -88,7 +177,14 @@ class SiamRPNTracker(SiameseTracker):
         z_crop = self.get_subwindow(img, self.center_pos,
                                     cfg.TRACK.EXEMPLAR_SIZE,
                                     s_z, self.channel_average)
+        
+        # TODO ME
+        # z_crop = get_subwindow_tracking_(img, self.center_pos,
+        #                                 cfg.TRACK.EXEMPLAR_SIZE,
+        #                                 s_z, self.channel_average)
         self.model.template(z_crop)
+        
+        
 
     def track(self, img):
         """
@@ -102,15 +198,26 @@ class SiamRPNTracker(SiameseTracker):
         s_z = np.sqrt(w_z * h_z)
         scale_z = cfg.TRACK.EXEMPLAR_SIZE / s_z
         s_x = s_z * (cfg.TRACK.INSTANCE_SIZE / cfg.TRACK.EXEMPLAR_SIZE)
-        x_crop = self.get_subwindow(img, self.center_pos,
+        
+        #print("Tracking...")
+        
+        # x_crop = self.get_subwindow(img, self.center_pos,
+        #                             cfg.TRACK.INSTANCE_SIZE,
+        #                             round(s_x), self.channel_average)
+        a = time.time()
+        x_crop = get_subwindow_tracking_(img, self.center_pos,
                                     cfg.TRACK.INSTANCE_SIZE,
-                                    round(s_x), self.channel_average)
-
+                                    round(s_x), self.channel_average).cuda().unsqueeze(0)
+        b = time.time()
+        print("get_subwindow_tracking time: " + str(b - a))
         outputs = self.model.track(x_crop)
 
         score = self._convert_score(outputs['cls'])
         pred_bbox = self._convert_bbox(outputs['loc'], self.anchors)
 
+        print(score)
+        print(pred_bbox)
+        
         def change(r):
             return np.maximum(r, 1. / r)
 
